74edfc83c92830e3bb6c5046daffe3cd89c25488
Youssef Abbas <youssefabbasawad@gmail.com>
2022-10-10 10:45:31 +0330

    data file added

---
+++ data.js
@@ -0,0 +1,49 @@
+const books = [
+  {
+    title: "Black Swan",
+    author: "6330ae7a0433fbd22957c2b5",
+    description: "About Black Swan",
+    price: 10,
+    cover: "soft cover",
+  },
+  {
+    title: "Skin In The Game",
+    author: "6330ae7a0433fbd22957c2b5",
+    description: "About Skin In The Game",
+    price: 12,
+    cover: "soft cover",
+  },
+  {
+    title: "Fooled By Randomness",
+    author: "6330ae7a0433fbd22957c2b5",
+    description: "About Fooled By Randomness",
+    price: 8,
+    cover: "hard cover",
+  },
+  {
+    title: "The Forty Rules Of Loves",
+    author: "6330ae8d0433fbd22957c2b7",
+    description: "About The Forty Rules Of Loves",
+    price: 14,
+    cover: "soft cover",
+  },
+  {
+    title: "The Island Of Missing Trees",
+    author: "6330ae8d0433fbd22957c2b7",
+    description: "About The Island Of Missing Trees",
+    price: 9,
+    cover: "soft cover",
+  },
+  {
+    title: "The Flea Palace",
+    author: "6330ae8d0433fbd22957c2b7",
+    description: "About The Flea Palace",
+    price: 10,
+    cover: "hard cover",
+  },
+];
+
+
+module.exports = {
+    books
+}
